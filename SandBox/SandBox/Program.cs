59217f7e640655af70a8511422b07828402241a3
ManuelSaleta <manuel.saleta@newmarketinc.com>
2020-09-18 11:52:02 -0400

    Adding Sandbox

---
+++ SandBox/SandBox/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] list = {10,-12345,300,450,25, 1534236469 };
+
+            foreach (var num in list)
+            {
+                Console.WriteLine($"Reverse of {num} is: {ReverseInteger.Reverse(num)}");
+            }
+        }
+    }
+}
